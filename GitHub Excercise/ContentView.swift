ccbfc5aa5dc029370123021efa87f4dde96116f6
wolfayet0855 <online_alt02@yahoo.com>
2024-07-01 12:34:27 -0400

    Initial Commit

---
+++ GitHub Excercise/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  GitHub Excercise
+//
+//  Created by user on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "4.circle")
+                .resizable()
+                .scaledToFit()
+            Text("Version 4.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            Rectangle()
+        }
+        .padding()
+        .foregroundColor(.purple)
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ GitHub Excercise/GitHub_ExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExcerciseApp.swift
+//  GitHub Excercise
+//
+//  Created by user on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
